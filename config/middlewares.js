5b6aa739b23f5bfcdf230da8bc1e9314e7f32636
Liu Zhongnuo <2313678365@qq.com>
2023-05-29 23:14:23 +0800

    中间件限制跨域

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,14 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      headers: "*",
+      origin: ["http://localhost:3000", "http://localhost:1337"],
+    }
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::logger',
