48d290f18706d8cab790ffa9e16769e479a799ff
Liu Zhongnuo <2313678365@qq.com>
2023-05-29 23:12:04 +0800

    layouts接口请求和返回进行过滤

--- src/api/layout/controllers/layout.js
+++ src/api/layout/controllers/layout.js
@@ -1,4 +1,5 @@
 'use strict';
+const {removeTime, removeAttrsAndId} = require("../../../utils/index");
 
 /**
  * layout controller
@@ -6,4 +7,13 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::layout.layout');
+module.exports = createCoreController('api::layout.layout', ({strapi}) => ({
+  async find(ctx) {
+    ctx.query = {
+      ...ctx.query,
+      populate: "deep",
+    };
+    const {data} = await super.find(ctx);
+    return removeAttrsAndId(removeTime(data[0]));
+  },
+}));
